6a100478fd375a3f1986669fc87a636772ee69ed
gjordan <gjordan@clio.eece.labs>
2009-10-18 14:23:18 -0400

    Current_PCB gets updated every 10 iterations

---
+++ sched.h
@@ -0,0 +1,14 @@
+#ifndef _SCHED_H
+#define _SCHED_H
+
+
+/* Scheduler
+ * This contains the function headers
+ * for the scheduler module, including
+ * the ready queue, and the wait queue
+ */
+
+u_int8_t sched();
+void BuildQueue(char NPID);
+
+#endif
