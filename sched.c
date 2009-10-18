0b00e5d30af2655cc8d0777108866890e7757abc
jgaudet <jgaudet@morpheus.eece.labs>
2009-10-18 13:34:43 -0400

    merging tips

--- sched.c
+++ sched.c
@@ -1,20 +1,45 @@
 /*
  *          File: sched.c
  *        Author: Gary S. Jordan
- *		  Zach Norris
- *		  Joe Gaudet
+ *		 		  Zach Norris
+ *		  		  Joe Gaudet
  * Last Modified:
  *         Topic:
  * ----------------------------------------------------------------
  *
  */
+//#include "Exec_Brain.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+
+#include "Exec_Brain.h"
 #include "sched.h"
 
-void * sched(struct ProcessControl * PCB, int rb, int io, curlyqueue_t * queue)
+static char* Queue_Start;
+static char* Queue_End;
+static char* Queue_Current;
+
+
+void BuildQueue(NPID)
 {
-	void * pid;
-	if (io == 0) {
-		pid = curlyqueue_dequeue(queue, PCB);
-		return pid;
-	}
+	Queue_Start=malloc(NPID);
+	Queue_End=Queue_Start+NPID;
+	Queue_Current=Queue_Start;
 }
+
+u_int8_t sched(PCB* ControlBlock, int io)
+{
+	Queue_Current++;
+	if (ControlBlock->B==1);
+	return PID;
+}
+
