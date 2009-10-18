d91cacb72f6b20f3424e0c8f199ae46c5237604d
gjordan <gjordan@clio.eece.labs>
2009-10-18 14:13:07 -0400

    type cast array pointer

---
+++ sched.h
@@ -0,0 +1,14 @@
+#ifndef _SCHED_H
+#define _SCHED_H
+
+
+/* Scheduler
+ * This contains the function headers
+ * for the scheduler module, including
+ * the ready queue, and the wait queue
+ */
+
+u_int8_t sched(PCB* ControlBlock, int io);
+void BuildQueue(char NPID);
+
+#endif
