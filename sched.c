3b0d6fa7ca973dcea508e18fa4482e52a7e86fab
znorris <znorris@aeolus.eece.labs>
2009-10-22 16:20:21 -0400

    starting philosopher

--- sched.c
+++ sched.c
@@ -57,17 +57,28 @@ int blockq(u_int8_t * pPID, int io)		//address of PID and I/O
 	u_int8_t * pid;
 	if (io == 0) {
 		curlyqueue_iterator_jump_to_front(bq);
+<<<<<<< /usr/students/znorris/cos431/proj3/brain09/sched.c
+		while (*pid != *pPID) {					//move through queue
+			pid = curlyqueue_get_value_at_iterator(bq,e);	//looking for specified
+			curlyqueue_iterator_step_forward(bq,e);		//PID in the queue
+=======
 
 		do
 		{
 			pid = curlyqueue_get_value_at_iterator(bq,e);
 	//		printf("Looking for one and found:  %x\n",pid);
 			curlyqueue_iterator_step_backward(bq,e);
+>>>>>>> /tmp/sched.c~other.HHhAco
 		}
+<<<<<<< /usr/students/znorris/cos431/proj3/brain09/sched.c
+		curlyqueue_delete_value_at_iterator(bq,e);		//pop that element
+		return *pid;						//return the PID
+=======
 		while (*pid != *pPID);
 
 
 
+>>>>>>> /tmp/sched.c~other.HHhAco
 	}
 	else {
 		curlyqueue_enqueue(bq, pPID);
