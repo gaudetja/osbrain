4c2b12e65f4b710c81d728c2b536792423c0c72f
znorris <znorris@zephir.eece.labs>
2009-10-29 19:40:43 -0400

    delorted

--- sched.c
+++ sched.c
@@ -58,8 +58,7 @@ int blockq(u_int8_t * pPID, int io)		//address of PID and I/O
 		do
 		{
 			pid = curlyqueue_get_value_at_iterator(bq,e);
-			if(*pid != *pPID) //printf("Looking for one and found:  %x\n",pid);
-			curlyqueue_iterator_step_backward(bq,e);
+			if(*pid != *pPID) curlyqueue_iterator_step_backward(bq,e);
 		}while (*pid != *pPID);
 		curlyqueue_delete_value_at_iterator(bq,e);		//pop that element
 		return *pid;						//return the PID
