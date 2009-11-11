1f681545bebf378bcd35b387b7cd5fb7634e6328
jgaudet <jgaudet@clio.eece.labs>
2009-11-10 20:09:37 -0500

    sharing

--- SharedMem.c
+++ SharedMem.c
@@ -1,1 +1,12 @@
+#include "Memory.h"
 
+#define sharedMEM 4*100
+
+static u_int32_t* SharedMemory_Start;					//Start of shared memory block
+static u_int32_t* SharedMemory_End;					//End of shared memory block
+static u_int16_t SharedMemory_Num = 0;				//number of elements in shared memory block
+
+void InitShared(int numshared) {
+	SharedMemory_Start=malloc(RAM);
+
+}
