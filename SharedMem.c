0552eaa2d7bc6096b647ff16eeb9c9a0036ca86c
jgaudet <jgaudet@zephir.eece.labs>
2009-11-12 01:46:31 -0500

    once again, leaving code compiling. Done for the morning need to add blocking routines to semnaphores.

--- SharedMem.c
+++ SharedMem.c
@@ -3,50 +3,58 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "Exec_Brain.h"
 #include "SharedMem.h"
 
-struct sema {
-	int value;
-	int PID;
-};
+
 
 
 
 #define numshared 100
 u_int32_t * shared;
-//sema semaphore;
+seman * semaphore;
 
 void InitShared(void) {
 	int i;
-	shared=calloc(numshared,4);
-	//semaphore=calloc(numshared,sizeof(semaphore));
 
+	shared=calloc(numshared,4);
+	semaphore = calloc(numshared,8);
+
+	//semaphore =tmp;
 	for(i=0;i<numshared;i++) {
-	//	semaphore[i]=1;
+		semaphore[i].PID=-1;
+		semaphore[i].value=1;
 	}
 }
 
 void PE(u_int8_t rand1, u_int8_t rand2) {
 	int i = 10*(rand1-48)+(rand2-48);
-	//if(semaphore->value) {
-i++;
-	//}
+	if(semaphore[i].value) {
+	 	semaphore[i].value=0;
+		semaphore[i].PID=Current_PCB->PID;
+
+	}
 }
+
 void VE(u_int8_t rand1, u_int8_t rand2) {
 	int i = 10*(rand1-48)+(rand2-48);
-	i++;
+	if(semaphore[i].value && (semaphore[i].PID==Current_PCB->PID)) {
+	 	semaphore[i].value=1;
+		semaphore[i].PID=-1;
+	}
 }
+
 void SI(u_int8_t rand1, u_int8_t rand2) {
 	int i = 10*(rand1-48)+(rand2-48);
-	i++;
+	semaphore[i].value=Current_PCB->R;
 }
+
 void LS(u_int8_t rand1, u_int8_t rand2) {
 	int i = 10*(rand1-48)+(rand2-48);
-	i++;
-	//Current_PCB->R = shared[i];
+	Current_PCB->R = shared[i];
 }
+
 void ST(u_int8_t rand1, u_int8_t rand2) {
 	int i = 10*(rand1-48)+(rand2-48);
-	i++;
-	//shared[i] = Current_PCB->R;
+	shared[i] = Current_PCB->R;
 }
--- SharedMem.h
+++ SharedMem.h
@@ -1,11 +1,20 @@
 #ifndef _SHAREDMEM_H
 #define _SHAREDMEM_H
 
+extern PCB * Current_PCB;
+
+typedef struct sema {
+	int value;
+	int PID;
+}seman;
+
 void InitShared(void);
 void PE(u_int8_t rand1, u_int8_t rand2);
 void VE(u_int8_t rand1, u_int8_t rand2);
 void SI(u_int8_t rand1, u_int8_t rand2);
 void LS(u_int8_t rand1, u_int8_t rand2);
 void ST(u_int8_t rand1, u_int8_t rand2);
+
+
 
 #endif
