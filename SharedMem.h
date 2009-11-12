5a9dce71d1e97bfe730184c1a3cece7e73482410
jgaudet <jgaudet@zephir.eece.labs>
2009-11-12 00:39:23 -0500

    (cartman voice)heh heh heh heh heh heh i made my shit compiiile

---
+++ SharedMem.h
@@ -0,0 +1,11 @@
+#ifndef _SHAREDMEM_H
+#define _SHAREDMEM_H
+
+void InitShared(void);
+void PE(u_int8_t rand1, u_int8_t rand2);
+void VE(u_int8_t rand1, u_int8_t rand2);
+void SI(u_int8_t rand1, u_int8_t rand2);
+void LS(u_int8_t rand1, u_int8_t rand2);
+void SS(u_int8_t rand1, u_int8_t rand2);
+
+#endif
