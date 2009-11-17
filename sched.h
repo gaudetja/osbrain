a152991f152bed657ac6b258d5d18aaaa324d9b8
gjordan <gjordan@morpheus.eece.labs>
2009-11-16 20:24:44 -0500

    Commented out a bunch

--- sched.h
+++ sched.h
@@ -1,15 +1,5 @@
 #ifndef _SCHED_H
 #define _SCHED_H
-/*
- *          File: sched.h
- *        Author: Gary S. Jordan
- *		  Zach Norris
- *		  Joe Gaudet
- * Last Modified:
- *         Topic:
- * ----------------------------------------------------------------
- *
- */
 
 int readyq(u_int8_t * pPID, char io);
 int blockq(u_int8_t * PID, int io);
