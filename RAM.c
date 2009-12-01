e128d33f6ad2aa425ce41ca5507b79281cbc55c9
znorris <znorris@asclepius.eece.labs>
2009-11-30 20:16:11 -0500

    redruM ReadRAM

---
+++ RAM.c
@@ -0,0 +1,65 @@
+/*
+ * PageMem.c
+ *
+ *      Author: Zach Norris
+ *      	Gary Jordan
+ *      	Joe Gaudet
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+
+#include "curlyqueue.h"
+#include "RAM.h"
+#include "Exec_Brain.h"
+#include "sched.h"
+
+WORDBYTES ReadRAM(u_int32_t location)
+{
+	WORDBYTES ReadWord;
+
+	ReadWord.word = RAM_Start[location];
+	if (ReadWord.word == 0) {
+		ReadWord.bytes.byte1 = '0';
+		ReadWord.bytes.byte2 = '0';
+		ReadWord.bytes.byte3 = '0';
+		ReadWord.bytes.byte4 = '0';
+	}
+	if (ReadWord.bytes.byte1 <= 0x39) {
+		u_int32_t Temp1 = (ReadWord.bytes.byte1 - '0') * 1000 +
+				  (ReadWord.bytes.byte2 - '0') * 100 +
+				  (ReadWord.bytes.byte3 - '0') * 10 +
+				  (ReadWord.bytes.byte4 - '0');
+		ReadWord.word = Temp1;
+	}
+	return ReadWord;
+}
+
+Load_Logical()
+{
+
+
+
+}
+
+Store_Logical()
+{
+
+
+}
+
+
+void WriteRAM(u_int32_t Value, u_int32_t Physical_Address)
+{
+	RAM_Start[Physical_Address]=Value;
+	return;
+}
+
