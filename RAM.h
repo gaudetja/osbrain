d69b9f5a228d5d1fca90b2852b2a51104b22164f
gjordan <gjordan@clio.eece.labs>
2009-11-30 20:36:24 -0500

    Working towards a build

---
+++ RAM.h
@@ -0,0 +1,35 @@
+/*
+ * PageMem.h
+ *
+ *  Created on: Nov 30, 2009
+ *      Author: Zach Norris
+ *      	Gary Jordan
+ *      	Joe Gaudet
+ */
+
+#ifndef RAM_H_
+#define RAM_H_
+
+
+typedef struct {
+	u_int32_t value;
+	char v;
+}cell;
+
+
+/*
+ * This function reads from RAM and returns the contents
+ * of RAM_Start at location *location*
+ */
+void Init_PageMem(int n, int m);
+WORDBYTES ReadRAM(u_int32_t location);
+
+/*
+ * This function writes value *Value* to location *Physical_Address*
+ */
+void WriteRAM(u_int32_t Value, u_int32_t Physical_Address);
+WORDBYTES WriteLogical(u_int32_t Value, u_int8_t rand1,u_int8_t rand2, u_int8_t PID);
+WORDBYTES ReadLogical(u_int8_t rand1,u_int8_t rand2, u_int8_t PID);
+
+
+#endif /* PAGEMEM_H_ */
