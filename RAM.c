a8e87cc9bbea0c88cbff1692422021d9dcc49c42
jgaudet <jgaudet@asclepius.eece.labs>
2009-12-01 16:40:40 -0500

    added page addal/removal

--- RAM.c
+++ RAM.c
@@ -71,6 +71,15 @@ WORDBYTES ReadLogical(u_int8_t rand1,u_int8_t rand2, u_int8_t PID)
 	return;
 
 }
+void InsertPage()
+{
+
+}
+
+void RemovePage()
+{
+
+}
 
 void WriteRAM(u_int32_t Value, u_int32_t Physical_Address)
 {
