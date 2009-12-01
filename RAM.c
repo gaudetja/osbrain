932df4af40e7e637c3aed2d8c2e66211eaeb85a5
gjordan <gjordan@clio.eece.labs>
2009-11-30 20:32:15 -0500

    Working towards a build

---
+++ RAM.c
@@ -0,0 +1,76 @@
+/*
+ * PageMem.c
+ *
+ *      Author: Zach Norris
+ *      	Gary Jordan
+ *      	Joe Gaudet
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+
+#include "curlyqueue.h"
+#include "RAM.h"
+#include "Exec_Brain.h"
+#include "sched.h"
+
+int * RAM_Start;
+int * RAM_End;
+
+
+
+
+
+Init_PageMem(int n, int m)
+{
+	RAM_Start = calloc(n,m);
+}
+
+WORDBYTES ReadRAM(u_int32_t location)
+{
+	WORDBYTES ReadWord;
+
+	ReadWord.word = RAM_Start[location];
+	if (ReadWord.word == 0) {
+		ReadWord.bytes.byte1 = '0';
+		ReadWord.bytes.byte2 = '0';
+		ReadWord.bytes.byte3 = '0';
+		ReadWord.bytes.byte4 = '0';
+	}
+	if (ReadWord.bytes.byte1 <= 0x39) {
+		u_int32_t Temp1 = (ReadWord.bytes.byte1 - '0') * 1000 +
+				  (ReadWord.bytes.byte2 - '0') * 100 +
+				  (ReadWord.bytes.byte3 - '0') * 10 +
+				  (ReadWord.bytes.byte4 - '0');
+		ReadWord.word = Temp1;
+	}
+	return ReadWord;
+}
+
+WORDBYTES WriteLogical(u_int32_t Value, u_int8_t rand1,u_int8_t rand2, u_int8_t PID)
+{
+
+
+
+}
+
+WORDBYTES ReadLogical(u_int8_t rand1,u_int8_t rand2, u_int8_t PID)
+{
+
+
+}
+
+void WriteRAM(u_int32_t Value, u_int32_t Physical_Address)
+{
+	RAM_Start[Physical_Address]=Value;
+	return;
+}
+
---
+++ RAM.h
@@ -0,0 +1,30 @@
+/*
+ * PageMem.h
+ *
+ *  Created on: Nov 30, 2009
+ *      Author: Zach Norris
+ *      	Gary Jordan
+ *      	Joe Gaudet
+ */
+
+#ifndef PAGEMEM_H_
+#define PAGEMEM_H_
+
+
+typedef struct {
+	u_int32_t value;
+	char v;
+}cell;
+
+
+/*
+ * This function reads from RAM and returns the contents
+ * of RAM_Start at location *location*
+ */
+WORDBYTES ReadRAM(u_int32_t location);
+void WriteRAM(u_int32_t Value, u_int32_t Physical_Address);
+WORDBYTES WriteLogical(u_int32_t Value, u_int8_t rand1,u_int8_t rand2, u_int8_t PID);
+WORDBYTES ReadLogical(u_int8_t rand1,u_int8_t rand2, u_int8_t PID);
+
+
+#endif /* PAGEMEM_H_ */
